1a1f4183f88e1ee2ddd526d5e15abd83b0cffccf
smissuser5 <a.mykhailovskyi@smissltd.com>
2021-09-27 00:39:04 +0300

    Add empty component if there are no employees exist

---
+++ laravel/resources/js/components/employees-page/includes/Empty.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="full-screen">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Empty",
+    props: {
+        message: {
+            type: String,
+            required: false,
+            default: 'No employees exist.'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.full-screen{
+    width: 100vw;
+    height: 100vh;
+    display:flex;
+    justify-content:center;
+    align-items: center
+}
+</style>
