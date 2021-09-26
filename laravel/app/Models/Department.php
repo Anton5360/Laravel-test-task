693cdee8a5de6fd726867c15126fad075f289fa6
smissuser5 <a.mykhailovskyi@smissltd.com>
2021-09-27 01:00:56 +0300

    Add php doc

--- laravel/app/Models/Department.php
+++ laravel/app/Models/Department.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -18,12 +19,27 @@ class Department extends Model
 
     protected $table = 'departments';
 
+    public $timestamps = false;
+
     protected $fillable = [
         'title'
     ];
 
+    /**
+     * One to many relationship with Employee model
+     * @return HasMany
+     */
     public function employees(): HasMany
     {
         return $this->hasMany(Employee::class, 'department_id', 'id');
+    }
+
+    /**
+     * Method returns all columns except for timestamps
+     * @return Collection
+     */
+    public static function allWithoutTimestamps(): Collection
+    {
+        return self::get(['id', 'title']);
     }
 }
--- laravel/app/Models/Employee.php
+++ laravel/app/Models/Employee.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 /**
  * @property int $id
@@ -24,6 +27,8 @@ class Employee extends Model
 {
     use HasFactory;
 
+    private const DEFAULT_EMPLOYEES_COUNT_PER_PAGE = 10;
+
     protected $table = 'employees';
 
     protected $fillable = [
@@ -39,8 +44,80 @@ class Employee extends Model
     ];
 
 
+    /**
+     * One to many relationship with Department model
+     * @return BelongsTo
+     */
     public function department(): BelongsTo
     {
         return $this->belongsTo(Department::class, 'department_id', 'id');
+    }
+
+    /**
+     * Method returns Paginator instance with all employees or employees only from some department
+     * @param Request $request
+     * @param int|null $departmentID
+     * @return LengthAwarePaginator
+     */
+    public static function getPaginateEmployeeList(Request $request, int $departmentID = null): LengthAwarePaginator
+    {
+        $query = Employee::query();
+
+        if($departmentID){
+            $query = $query->where('department_id', $departmentID);
+        }
+
+        return $query->with('department')
+            ->paginate(
+                self::getNeededEmployeesCount($request->get('quantity')),
+                ['*'],
+                'page',
+                $request->get('page'));
+    }
+
+    /**
+     * Method prepares paginated employees array.
+     * Exactly this one changes salary of employees in paginated array
+     * @param array $items
+     * @return array
+     */
+    public static function getPreparedPaginatedItems(array $items): array
+    {
+        foreach($items as $item){
+            $item['salary'] = self::countEmployeeSalary(
+                                                    $item['salary'],
+                                                    $item['work_hours'],
+                                                    $item['salary_type']
+                                                );
+        }
+
+        return $items;
+    }
+
+    /**
+     * If there is no quantity get parameter, method returns default number of employees per page
+     * @param int|null $count
+     * @return int
+     */
+    private static function getNeededEmployeesCount(?int $count): int
+    {
+        return $count ?? self::DEFAULT_EMPLOYEES_COUNT_PER_PAGE;
+    }
+
+
+    /**
+     * Method returns counted employee`s salary
+     * @param float $salary
+     * @param float $workHours
+     * @param string $salaryType
+     * @return string
+     */
+    private static function countEmployeeSalary(float $salary, float $workHours, string $salaryType): string
+    {
+        $countedSalary = $salaryType === 'hourly'
+                        ? $salary * $workHours
+                        : $salary;
+
+        return number_format($countedSalary, 2);
     }
 }
